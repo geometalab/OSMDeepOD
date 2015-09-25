abb428b90d8ef1840a482bb0d901afa980de5840
samuel.kurath@gmail.com <murthy@skurath.skurath>
2015-09-25 09:38:06 +0200

    Wrote Test to PositionConverter

--- src/service/PositionConverter.py
+++ src/service/PositionConverter.py
@@ -8,18 +8,18 @@ class PositionConverter:
     def __init__(self):
         # Meters/Pixel = 0.30 --> Zoomlevel 19
         # https://blogs.bing.com/maps/2006/02/25/map-control-zoom-levels-gt-resolution/
-        self.SCALE = 0.30
+        self.PIXEL_TO_METER_SCALE = 0.30
         self.IMAGE_SIZE = 350
         self.MIDLE_EARTH_RADIUS_IN_METER = 6378388.0#6371000.785#6378388#
 
     def getCoordinate(self, startPoint, xImage, yImage):
         x = self.getMeter(xImage)
         y= self.getMeter(yImage)
-        endPoint = Point(startPoint.latitude + x, startPoint.longitude + y)
+        endPoint = self.addDistanceToPoint(startPoint,x,y)
         return endPoint
 
     def getMeter(self, pixel):
-        return pixel * self.SCALE
+        return pixel * self.PIXEL_TO_METER_SCALE
 
 
 
--- tests/service/TestPositionConverter.py
+++ tests/service/TestPositionConverter.py
@@ -1,18 +1,36 @@
 import unittest
 from service.PositionConverter import PositionConverter
 from geopy import Point
-
+import cv2
 
 class TestPositionConverter(unittest.TestCase):
 
     def testAddDistance(self):
         positionConverter = PositionConverter()
         startPoint = Point(47.2, 8.81, 0)
+
         newPoint = Point(startPoint.latitude, startPoint.longitude)
         newPoint = positionConverter.addDistanceToPoint(newPoint, 3000, 4000)
-
         distance = positionConverter.getDistantBetweenPoinsInMeters(startPoint, newPoint)
-        print distance
 
         self.assertTrue(distance > 4995 and distance < 5005)
+
+    def testImageWidth(self):
+        positionConverter = PositionConverter()
+        orthophotoPath = './orthofotos/47.2246376_8.8178977.jpg'
+        orthophoto = cv2.imread(orthophotoPath,0)
+        height, width = orthophoto.shape[:2]
+        startPoint = Point(47.2246376, 8.8178977, 0)
+
+        endPoint = Point(startPoint.latitude, startPoint.longitude)
+        endPoint = positionConverter.getCoordinate(endPoint,350,0)
+        distance = positionConverter.PIXEL_TO_METER_SCALE * width
+        difference = positionConverter.getDistantBetweenPoinsInMeters(startPoint, endPoint)
+
+        print "distance: " + str(distance)
+        print "difference: " + str(difference)
+        print width
+        print height
+        self.assertTrue(distance+5 > difference and difference > distance-5)
+
 
