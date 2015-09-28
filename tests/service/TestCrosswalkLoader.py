fec7fd1d84fa26bf92cd92c8d79573f4b5d24539
Murthy10 <samuel.kurath@gmail.com>
2015-09-28 09:01:33 +0200

    added getCrosswalksByPositions()

--- tests/service/TestCrosswalkLoader.py
+++ tests/service/TestCrosswalkLoader.py
@@ -1,10 +1,21 @@
 import unittest
 from service.CrosswalkLoader import CrosswalkLoader
+from geopy import Point
 
 class TestCrosswalkLoader(unittest.TestCase):
 
-    def testDownload(self):
+    def testGetCrosswalkPositions(self):
         crosswalkLoader = CrosswalkLoader()
         points = crosswalkLoader.getCrosswalkPositions()
+        self.assertTrue(len(points) > 0)
+
+
+    def testGetCrosswalksByPositions(self):
+        crosswalkLoader = CrosswalkLoader()
+        downLeftPoint = Point('47.22491209728128','8.815191135900864')
+        upRightPoint = Point('47.22819078179419','8.823774204748178')
+
+        points = crosswalkLoader.getCrosswalksByPositions(downLeftPoint, upRightPoint)
 
         self.assertTrue(len(points) > 0)
+
