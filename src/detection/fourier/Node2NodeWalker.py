cf3785f5275b28ca7fa84f83fc026ede1948818e
Sebubu <sevi_buehler@hotmail.com>
2015-10-06 16:50:40 +0200

    added BBox19 conversation

---
+++ src/detection/fourier/Node2NodeWalker.py
@@ -0,0 +1,9 @@
+from src.service.ImagePlotter import ImagePlotter
+
+class StreetWalker:
+    def __init__(self, street, images):
+        self.street = street
+        self.images = images
+
+    def walk(self):
+        print "walk"
