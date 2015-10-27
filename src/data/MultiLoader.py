67f48de4a829f2a91da2f2199200f2d8f142cce0
Murthy10 <samuel.kurath@gmail.com>
2015-10-27 15:25:18 +0100

    Multiprocessing

---
+++ src/data/MultiLoader.py
@@ -0,0 +1,32 @@
+from multiprocessing.dummy import Pool as ThreadPool
+import urllib2
+import StringIO
+from PIL import Image
+
+class MultiLoader:
+    def __init__(self):
+        self.urls = []
+        self.results = []
+
+    @classmethod
+    def from_url_list(cls, urls):
+        loader = cls()
+        loader.urls = urls
+        return loader
+
+    def download(self):
+        pool = ThreadPool(100)
+        results = pool.map(urllib2.urlopen, self.urls)
+        pool.close()
+        pool.join()
+
+        self.results = self._convert_to_image(results)
+
+    def _convert_to_image(self, results):
+        ret = []
+        for loads in results:
+            img = loads.read()
+            img = Image.open(StringIO.StringIO(img))
+            ret.append(img)
+
+        return ret
--- src/data/TileLoader.py
+++ src/data/TileLoader.py
@@ -5,6 +5,7 @@ from src.data.globalmaptiles import GlobalMercator
 from src.base.Constants import Constants
 from src.base.Bbox import Bbox
 from src.base.Tile import Tile
+from src.data.MultiLoader import MultiLoader
 
 class TileLoader:
     def __init__(self, bbox):
@@ -13,21 +14,21 @@ class TileLoader:
         self.POSTLINK = '.jpeg?g=4401&n=z'
 
     def getTiles(self):
-        return self.__download_tiles(self.bbox)
+        return self._download_tiles(self.bbox)
 
-    def __url_to_image(self, url):
+    def _url_to_image(self, url):
         resp, content = httplib2.Http().request(url)
         image = Image.open(StringIO(content))
         return image
 
-    def __build_url(self, quadtree):
+    def _build_url(self, quadtree):
         return self.PRELINK + str(quadtree) + self.POSTLINK
 
-    def __download_image(self, quadtree):
-        url = self.__build_url(quadtree)
-        return self.__url_to_image(url)
+    def _download_image(self, quadtree):
+        url = self._build_url(quadtree)
+        return self._url_to_image(url)
 
-    def __download_tiles(self, bbox):
+    def _download_tiles(self, bbox):
         mercator = GlobalMercator()
         mminx, mminy = mercator.LatLonToMeters(bbox.bottom, bbox.left)
         mmaxx, mmaxy = mercator.LatLonToMeters(bbox.top, bbox.right)
@@ -35,18 +36,32 @@ class TileLoader:
         tminx, tminy = mercator.MetersToTile( mminx, mminy, Constants.ZOOM)
         print tminx
         print tmaxx
+        urls = []
+        row = 0
+        for ty in range(tminy, tmaxy+1):
+            for tx in range(tminx, tmaxx+1):
+                #tilefilename = "%s/%s/%s" % (Constants.ZOOM, tx, ty)
+                quadtree = mercator.QuadTree(tx, ty, Constants.ZOOM)
+                url = self._build_url(quadtree)
+                urls.append(url)
+
+        loader = MultiLoader.from_url_list(urls)
+        loader.download()
+
+        i = 0
         tiles = []
         row = 0
         for ty in range(tminy, tmaxy+1):
             tiles.append([])
             for tx in range(tminx, tmaxx+1):
                 #tilefilename = "%s/%s/%s" % (Constants.ZOOM, tx, ty)
-                quadtree = mercator.QuadTree(tx, ty, Constants.ZOOM)
-                image = self.__download_image(quadtree)
+
+                image = loader.results[i]
                 left, bottom, top, right = mercator.TileBounds(tx, ty, Constants.ZOOM)
                 bbox = Bbox(left, bottom, top, right)
                 tile = Tile(image, bbox)
                 tiles[row].append(tile)
+                i += 1
             row += 1
         return tiles
 
