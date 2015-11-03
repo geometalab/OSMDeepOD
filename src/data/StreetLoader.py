1969407d574e491e80bedcc7c4d44b43b0983f1f
Murthy10 <samuel.kurath@gmail.com>
2015-11-03 19:02:58 +0100

    streetloader with multiloader

--- src/data/StreetLoader.py
+++ src/data/StreetLoader.py
@@ -9,21 +9,21 @@ class StreetLoader:
                                     'primary_link', 'secondary_link', 'tertiary_link', 'pedestrian']
 
     def _build_query(self, tag, bbox):
+        sbox = self._bbox_to_string(bbox)
         query = '[out:json];(node["highway"="'
-        query += str(tag)
+        query += tag
         query += '"]('
-        query += str(bbox)
+        query += sbox
         query += '); way["highway"="'
-        query += str(tag)
+        query += tag
         query += '"]('
-        query += str(bbox)
+        query += sbox
         query += '); relation["highway"="'
-        query += str(tag)
+        query += tag
         query += '"]('
-        query += str(bbox)
+        query += sbox
         query += '); out body;  >; out skel qt;'
         return query
-
 
     def _get_queries(self, bbox):
         queries = []
@@ -31,9 +31,21 @@ class StreetLoader:
             queries.append(self._build_query(tag, bbox))
         return queries
 
+    def _build_streets(self, ways):
+        streets = []
+        for way in ways:
+            street = Street.from_info(way.tags.get("name", "n/a"),way.id, way.tags.get("highway", "n/a"))
+            for node in way.nodes:
+                street.nodes.append(Node(node.lat, node.lon, node.id))
+            streets.append(street)
+        return streets
+
     def load_streets(self, bbox):
         queries = self._get_queries(bbox)
         loader = MultiStreetLoader.from_query_list(queries)
         ways = loader.download()
-        return ways
+        streets = self._build_streets(ways)
+        return streets
 
+    def _bbox_to_string(self,bbox):
+        return str(bbox.top)+','+str(bbox.left)+','+str(bbox.bottom)+','+str(bbox.right)
