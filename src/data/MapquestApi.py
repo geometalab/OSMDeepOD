56c32befab08d499afaaf5749bf331b258559c4a
Murthy10 <samuel.kurath@gmail.com>
2015-11-04 14:53:11 +0100

    refactored StreetLoader

--- src/data/MapquestApi.py
+++ src/data/MapquestApi.py
@@ -5,13 +5,12 @@ class MapquestApi:
     #"http://open.mapquestapi.com/xapi/api/0.6/node[highway=crossing][bbox=8.815191135900864,47.22491209728128,8.823774204748178,47.22819078179419]?key=YKqJ7JffQIBKyTgALLNXLVrDSaiQGtiI"
     def __init__(self, developerKey = "YKqJ7JffQIBKyTgALLNXLVrDSaiQGtiI"):
         self.developerKey = developerKey
-        self.__LINK_PREFIX = "http://open.mapquestapi.com/xapi/api/0.6/way[%TAG%][bbox="
+        self.__LINK_PREFIX = "http://open.mapquestapi.com/xapi/api/0.6/way[highway=*][bbox="
         self.__LINK_POSTFIX = "]?key="
 
-    def request(self, tag, box):
-        prefix = self.__LINK_PREFIX.replace("%TAG%",tag)
+    def request(self, box):
         postfix = self.to_mapquest_format(box) + self.__LINK_POSTFIX + self.developerKey
-        url = prefix + postfix
+        url = self.__LINK_PREFIX + postfix
         resp, content = httplib2.Http().request(url)
         return ElementTree.fromstring(content)
 
--- src/data/StreetLoader.py
+++ src/data/StreetLoader.py
@@ -11,27 +11,28 @@ class StreetLoader:
                                    'unclassified', 'residential', 'service', 'trunk_link',
                                    'primary_link', 'secondary_link', 'tertiary_link', 'pedestrian']
 
-    def load_streets(self, box):
-        result = []
-        for categorie in self._STREET_CATEGORIES:
-            tag = self._ATTRIBNAME + "=" + categorie
-            tree = self.api.request(tag, box)
-            streets = self._parse_tree(tree, box)
-            result = result + streets
-        return result
-
-    def _parse_tree(self, tree, bbox):
-        nodesDict = self._getNodesDict(tree, bbox)
-        streets = []
-        for way in tree.iter('way'):
-            results = self._parse_way(way, nodesDict, bbox)
-            for res in results:
-                streets.append(res)
+    def load_streets(self, bbox):
+        tree = self.api.request(bbox)
+        streets = self._parse_tree(tree, bbox)
         return streets
 
-    def _parse_way(self, way, nodesDict, bbox):
+    def _parse_tree(self, tree, bbox):
+        node_map = self._get_nodes_map(tree, bbox)
+        streets = []
+        for way in tree.iter('way'):
+            for tag in way.iter('tag'):
+                for category in self._STREET_CATEGORIES:
+                    if self._is_in_category(tag,category):
+                        results = self._parse_way(way, node_map, bbox)
+                        streets += results
+        return streets
+
+    def _is_in_category(self,tag, category):
+        return str(tag.attrib) == "{'k': 'highway', 'v': '" + category + "'}"
+
+    def _parse_way(self, way, node_map, bbox):
         result = []
-        nodes = self._createNodeList(way, nodesDict)
+        nodes = self._create_node_list(way, node_map)
         borderdBox = bbox.getBboxExludeBorder(10)
         for i in range(len(nodes) -1):
             me = nodes[i]
@@ -60,7 +61,7 @@ class StreetLoader:
         street = Street.from_info(name,ident,highway)
         return street
 
-    def _createNodeList(self, way, nodesDict):
+    def _create_node_list(self, way, nodesDict):
         nodes = []
         for node in way.iter('nd'):
             nid = node.get('ref')
@@ -68,7 +69,7 @@ class StreetLoader:
                 nodes.append(nodesDict[nid])
         return nodes
 
-    def _getNodesDict(self,tree, bbox):
+    def _get_nodes_map(self,tree, bbox):
         nodes = {}
         for node in tree.iter('node'):
             ident = node.get('id')
