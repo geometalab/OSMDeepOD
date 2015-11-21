d973ac8eeef524b444d32a1459d52907dc4bae45
Sebubu <sevi_buehler@hotmail.com>
2015-11-21 13:50:31 +0100

    refactored Convnet.py

--- src/detection/deep/Convnet.py
+++ src/detection/deep/Convnet.py
@@ -9,119 +9,141 @@ import theano
 import os
 import numpy as np
 
-def convnet55():
 
-    # input image dimensions
-    img_rows, img_cols = 50, 50
-    # number of convolutional filters to use
-    nb_filters1 = 48
-    nb_filters2 = 128
-    nb_filters3 = 256
-    # size of pooling area for max pooling
-    nb_pool = 2
-    # convolution kernel size
-    nb_conv = 3
-    #image is rgb
-    img_channels = 3
+class Convnet:
+    def __init__(self):
+        self.verbose = True
+        self.very_verbose = False
+        self.model = None
+        self.hdf5_file = "convnet48.e158-l0.055.hdf5"
+        self.threshold = 0.9
 
-    #Lamda for L2 regularization
-    lmda_dense = 8e-5
-    lmda_conv = 8e-5
+    @classmethod
+    def from_verbose(cls, verbose=True):
+        convnet = cls()
+        convnet.verbose = verbose
+        return convnet
 
+    def _out(self, msg):
+        if self.verbose:
+            print(msg)
 
-    print("put convnet together")
-    model = Sequential()
+    def initialize(self):
+        self._enable_multithreading()
+        self._out("Compile convnet")
+        self.model = self._compile_model()
+        self._out("Load " + self.hdf5_file)
+        self._load_weights(self.hdf5_file)
 
-    model.add(Convolution2D(nb_filters1, nb_conv , nb_conv, W_regularizer=l2(lmda_conv), border_mode='full', input_shape=(img_channels, img_rows, img_cols)))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(nb_filters1, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    def is_initialized(self):
+        return not self.model is None
 
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(nb_filters2, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(nb_filters2, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    def predict_crosswalks(self, pil_image_list):
+        x = np.zeros((len(pil_image_list), 50, 50, 3))
+        for i in range(len(pil_image_list)):
+            img = np.array(pil_image_list[i])
+            x[i] = img
+        x = x.reshape(x.shape[0], 3, 50, 50)
+        x = x.astype("float32")
+        x /= 255
+        results = self._predict_list(x)
+        return results
 
-    model.add(Dropout(0.5))
-    model.add(Convolution2D(nb_filters3, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(nb_filters3, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(nb_filters3, nb_conv, nb_conv))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    def _predict_list(self, x):
+        predictions = self.model.predict(x)
+        results = []
+        for predict in predictions:
+            isCrosswalk =  predict[0] > self.threshold
 
-    model.add(Flatten())
+            if self.very_verbose:
+                if isCrosswalk:
+                    print ("Zebra: " + str(predict))
+                else:
+                    print(str(predict))
 
-    model.add(Dropout(0.5))
-    model.add(Dense(2048, W_regularizer=l2(lmda_dense)))
-    model.add(Activation('relu'))
+            results.append(isCrosswalk)
+        return results
 
-    model.add(Dropout(0.5))
-    model.add(Dense(1024, W_regularizer=l2(lmda_dense)))
-    model.add(Activation('relu'))
-
-    model.add(Dropout(0.5))
-    model.add(Dense(2, W_regularizer=l2(lmda_dense)))
-    model.add(Activation('softmax'))
-
-    print("start compiling")
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
-    current_dir = os.path.join(os.getcwd(), os.path.dirname(__file__))
-    #Best Net 64f4:
-    network_path = ""
-    if os.path.exists('/root/OSM-Crosswalk-Detection/DockerIam'):
-        network_path = '/root/OSM-Crosswalk-Detection/src/detection/deep/convnet48.e158-l0.055.hdf5'
-    else:
-        network_path = current_dir + "/" + "convnet48.e158-l0.055.hdf5"
+    def _enable_multithreading(self):
+        theano.config.openmp = True
 
 
-    model.load_weights(network_path)
-    return model
+    def _compile_model(self):
+        # input image dimensions
+        img_rows, img_cols = 50, 50
+        # number of convolutional filters to use
+        nb_filters1 = 48
+        nb_filters2 = 128
+        nb_filters3 = 256
+        # size of pooling area for max pooling
+        nb_pool = 2
+        # convolution kernel size
+        nb_conv = 3
+        #image is rgb
+        img_channels = 3
 
-def predictCrosswalks(pilimg_list):
-    x = np.zeros((len(pilimg_list), 50, 50, 3))
-    for i in range(len(pilimg_list)):
-        img = np.array(pilimg_list[i])
-        x[i] = img
-    x = x.reshape(x.shape[0], 3, 50, 50)
-    x = x.astype("float32")
-    x /= 255
-    results = _predict_list(x)
-    return results
+        #Lamda for L2 regularization
+        lmda_dense = 8e-5
+        lmda_conv = 8e-5
 
-def _predict_list(x):
-    global last_prediction
-    predictions = network.predict(x)
-    results = []
-    for predict in predictions:
-        #isCrosswalk = predict[0] > 0.999 and predict[1] < 1e-300
-        #isCrosswalk =  predict[1] < 1e-20
-        isCrosswalk =  predict[0] > 0.9
-        #if(isCrosswalk): print("Zerba " + str(predict))
-        #else: print(str(predict))
-        results.append(isCrosswalk)
-    last_prediction = predictions
-    return results
+        model = Sequential()
 
-network = None
-last_prediction = None
+        model.add(Convolution2D(nb_filters1, nb_conv , nb_conv, W_regularizer=l2(lmda_conv), border_mode='full', input_shape=(img_channels, img_rows, img_cols)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+        model.add(Convolution2D(nb_filters1, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 
-def initialize():
-    global network
-    if not network is None: return
-    _enable_keras_multithreading()
+        model.add(Dropout(0.5))
+        model.add(Convolution2D(nb_filters2, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+        model.add(Convolution2D(nb_filters2, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 
-    #network = _load_64f4c(True)
-    network = convnet55()
-    #Schwellwert: 1e-150, isCrosswalk = predict[0] > 0.9 and predict[1] < 1e-150
+        model.add(Dropout(0.5))
+        model.add(Convolution2D(nb_filters3, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+        model.add(Convolution2D(nb_filters3, nb_conv, nb_conv, W_regularizer=l2(lmda_conv)))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.2))
+        model.add(Convolution2D(nb_filters3, nb_conv, nb_conv))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 
-def _enable_keras_multithreading():
-    theano.config.openmp = True
+        model.add(Flatten())
+
+        model.add(Dropout(0.5))
+        model.add(Dense(2048, W_regularizer=l2(lmda_dense)))
+        model.add(Activation('relu'))
+
+        model.add(Dropout(0.5))
+        model.add(Dense(1024, W_regularizer=l2(lmda_dense)))
+        model.add(Activation('relu'))
+
+        model.add(Dropout(0.5))
+        model.add(Dense(2, W_regularizer=l2(lmda_dense)))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+        return model
+
+    def _load_weights(self, hdf5_file):
+        current_dir = os.path.join(os.getcwd(), os.path.dirname(__file__))
+
+        network_path = ""
+        if self._is_docker_container():
+            #Docker makes some crucial tricks in the filesystem virtualization. If it's a docker then we use this path
+            network_path = '/root/OSM-Crosswalk-Detection/src/detection/deep/' + hdf5_file
+        else:
+            network_path = current_dir + "/" + hdf5_file
+
+        self.model.load_weights(network_path)
+
+    def _is_docker_container(self):
+        return os.path.exists('/root/OSM-Crosswalk-Detection/DockerIam')
+
+
