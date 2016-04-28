3bf20399795bf6b9b1bb7f23c27ce2840d6a828c
Marcel Huber <marcelhuberfoo@gmail.com>
2016-04-28 10:13:51 +0200

    using argparse module instead of handwritten argument parsing
    - usage of command line options/arguments changed
    - adapted corresponding Dockerfile commands and READMEs
    - set version of module to 0.1.0
    
    closes #40

--- src/role/main.py
+++ src/role/main.py
@@ -1,74 +1,133 @@
+import argparse
+from src.base.Bbox import Bbox
+from src.role.Worker import Worker
+from src.role.Manager import Manager
+from redis.exceptions import ConnectionError
+from src import cwenv
+
+
+def redis_args(args):
+    return [args.redis_host, args.redis_port, args.redis_pass]
+
+
+def manager(args):
+    big_bbox = Bbox.from_lbrt(
+        args.bb_left,
+        args.bb_bottom,
+        args.bb_right,
+        args.bb_top)
+    try:
+        print('Manger has started...')
+        Manager.from_big_bbox(big_bbox, redis_args(args))
+    except ConnectionError:
+        print(
+            'Failed to connect to redis instance [{ip}:{port}], is it running? Check connection arguments and retry.'.format(
+                ip=args.redis_host,
+                port=args.redis_port))
+    finally:
+        print 'Manager has finished!'
+
+
+def jobworker(args):
+    jobWorker = Worker.from_worker(['jobs'])
+    try:
+        print 'JobWorker has started...'
+        jobWorker.run(redis_args(args))
+    except ConnectionError:
+        print(
+            'Failed to connect to redis instance [{ip}:{port}], is it running? Check connection arguments and retry.'.format(
+                ip=args.redis_host,
+                port=args.redis_port))
+    finally:
+        print 'JobWorker has finished!'
+
+
+def resultworker(args):
+    resultWorker = Worker.from_worker(['results'])
+    try:
+        print 'ResultWorker has started...'
+        resultWorker.run(redis_args(args))
+    except ConnectionError:
+        print(
+            'Failed to connect to redis instance [{ip}:{port}], is it running? Check connection arguments and retry.'.format(
+                ip=args.redis_host,
+                port=args.redis_port))
+    finally:
+        print 'ResultWorker has finished!'
+
+
+def mainfunc():
+    parser = argparse.ArgumentParser(description='Detect crosswalks.', )
+    redis_host = cwenv('REDIS_HOST')
+    redis_port = cwenv('REDIS_PORT')
+    redis_pass = cwenv('REDIS_PASS')
+    parser.add_argument(
+        '--redis',
+        action='store',
+        dest='redis_host',
+        default=redis_host,
+        help='hostname or ip of redis database, default is ' +
+        redis_host)
+    parser.add_argument(
+        '--port',
+        action='store',
+        dest='redis_port',
+        default=redis_port,
+        help='port of redis database, default is ' +
+        redis_port)
+    parser.add_argument(
+        '--pass',
+        action='store',
+        dest='redis_pass',
+        default=redis_pass,
+        help='password of redis database, default is ' +
+        redis_pass)
+
+    subparsers = parser.add_subparsers(
+        title='worker roles',
+        description='',
+        help='Select the role of this process')
+
+    p_manager = subparsers.add_parser(
+        'manager',
+        help='Splits up the given bounding box (WGS84, minlon/minlat/maxlon/maxlat) into small pieces and puts them into the redis queue to be consumed by the jobworkers.')
+
+    bb_left, bb_bottom, bb_right, bb_top = None, None, None, None
+    p_manager.add_argument(
+        'bb_left',
+        type=float,
+        action='store',
+        help='left float value of the bounding box (WGS84, minlon)')
+    p_manager.add_argument(
+        'bb_bottom',
+        type=float,
+        action='store',
+        help='bottom float value of the bounding box (WGS84, minlat)')
+    p_manager.add_argument(
+        'bb_right',
+        type=float,
+        action='store',
+        help='right float value of the bounding box (WGS84, maxlon)')
+    p_manager.add_argument(
+        'bb_top',
+        type=float,
+        action='store',
+        help='top float value of the bounding box (WGS84, maxlat)')
+    p_manager.set_defaults(func=manager)
+
+    p_jobworker = subparsers.add_parser(
+        'jobworker',
+        help='Detect crosswalks on element from the redis queue.')
+    p_jobworker.set_defaults(func=jobworker)
+
+    p_resultworker = subparsers.add_parser(
+        'resultworker',
+        help='Consolidate and write out results.')
+    p_resultworker.set_defaults(func=resultworker)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
 if __name__ == "__main__":
-    import sys
-    from src.base.Bbox import Bbox
-    from src.role.Worker import Worker
-    from src.role.Manager import Manager
-    from redis import Redis
-
-    def Usage(s = ""):
-        print "Usage: main.py --redis REDIS_IP_ADDR --role 'manager' left bottom right top |'jobworker' | 'resultworker' "
-        print
-        if s:
-            print s
-            print
-        print "OSM-Crosswalk-Detection is a tool to extract the position of crosswalks on orthophotos."
-        sys.exit(1)
-
-    def _get_float(arg):
-        try:
-            return float(arg)
-        except ValueError:
-            Usage("ERROR: All boundingbox parameters needs to be floats")
-
-    bottom, left, top, right = None, None, None, None
-    argv = sys.argv
-    if len(argv) < 5:
-        Usage("ERROR: You have to specify all needed arguments.")
-    i = 1
-    redis_ip = ''
-    if argv[i] == '--redis':
-        i += 1
-        redis_ip = argv[i]
-        redis = [str(redis_ip), '40001', 'crosswalks']
-        i += 1
-
-    role = ''
-    if argv[i] == '--role':
-        i += 1
-        role = argv[i]
-        i += 1
-
-    if role == 'manager':
-        if len(argv) < 7:
-            Usage("ERROR: You have to specify the whole boundingbox.")
-        left = _get_float(argv[i])
-        i += 1
-        bottom = _get_float(argv[i])
-        i += 1
-        right = _get_float(argv[i])
-        i += 1
-        top = _get_float(argv[i])
-
-        if left is None or bottom is None or right is None or top is None:
-            Usage("ERROR: You have to specify left, bottom, right and top .")
-        if top < bottom:
-            Usage("ERROR: 'top' must be bigger then 'bottom'")
-        if right < left:
-            Usage("ERROR: 'right' must be bigger then 'left'")
-        big_bbox = Bbox.from_lbrt(left, bottom, right, top)
-        print 'Manger is running!'
-        Manager.from_big_bbox(big_bbox, redis)
-        print 'Manger is finished!'
-    elif role == 'jobworker':
-        print 'JobWorker is running!'
-        jobWorker = Worker.from_worker(['jobs'])
-        jobWorker.run(redis)
-    elif role == 'resultworker':
-        print 'ResultWorker is running!'
-        resultWorker = Worker.from_worker(['results'])
-        resultWorker.run(redis)
-    else:
-        Usage("ERROR: Sorry, given role is not implemented yet.")
-
-
-
+    mainfunc()
