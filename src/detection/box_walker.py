fed5806a71e83a09fcf4acd05f2fdcef73f8954d
Murthy10 <samuel.kurath@gmail.com>
2016-09-01 14:59:18 +0200

    fixed Search() initialisation in box_walker

--- src/detection/box_walker.py
+++ src/detection/box_walker.py
@@ -15,12 +15,12 @@ from src.data.osm_comparator import OsmComparator
 
 class BoxWalker:
     def __init__(self, bbox, search=None):
+        self.search = Search() if search is None else search
         self.bbox = FittingBbox(zoom_level=search.zoom_level).get(bbox)
         self.tile = None
         self.streets = []
         self.convnet = None
         self.logger = logging.getLogger(__name__)
-        self.search = Search() if search is None else search
         self.square_image_length = 50
         self.max_distance = self._calculate_max_distance(search.zoom_level, self.square_image_length)
 
