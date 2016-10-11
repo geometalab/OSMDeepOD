ad1f535d1fb15548b7aa25dc18e02b484cc1e654
Murthy10 <samuel.kurath@gmail.com>
2016-10-11 13:40:21 +0200

    removed redis pool

--- src/role/worker_functions.py
+++ src/role/worker_functions.py
@@ -1,5 +1,5 @@
 from rq import Queue
-from redis import Redis, ConnectionPool
+from redis import Redis
 import json
 import os
 
@@ -7,13 +7,11 @@ from src import cwenv
 from src.detection.box_walker import BoxWalker
 
 
-def enqueue_results(result_nodes, redis_connection_pool):
-    redis_queue_connection = Redis(connection_pool=redis_connection_pool)
-    q = Queue('results', connection=redis_queue_connection)
+def enqueue_results(result_nodes, redis_connection):
+    q = Queue('results', connection=redis_connection)
     q.enqueue_call(func=store, args=(result_nodes,), timeout=5400)
-    redis_result_connection = Redis(connection_pool=redis_connection_pool)
     for result_node in result_nodes:
-        redis_result_connection.rpush('visualizing', result_node.to_geojson())
+        redis_connection.rpush('visualizing', result_node.to_geojson())
 
 
 def detect(bbox, redis, search):
@@ -24,8 +22,8 @@ def detect(bbox, redis, search):
         walker.load_convnet()
         walker.load_tiles()
         crosswalk_nodes = walker.walk()
-    redis_connection_pool = ConnectionPool(redis[0], redis[1], password=redis[2])
-    enqueue_results(crosswalk_nodes, redis_connection_pool)
+    redis_connection = Redis(redis[0], redis[1], password=redis[2])
+    enqueue_results(crosswalk_nodes, redis_connection)
 
 
 PATH_TO_CROSSWALKS = os.path.join(cwenv('OUTPUT_DIR', default='.'), 'crosswalks.json')
