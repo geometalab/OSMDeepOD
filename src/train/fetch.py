06c2a1100ae1897fd4ca72f515d687580d3bbec7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-06-20 19:08:44 +0200

    fix train fetch.py

--- src/train/fetch.py
+++ src/train/fetch.py
@@ -11,11 +11,11 @@ from src.train.osm_object_walker import OsmObjectWalker
 import argparse
 
 def main(args):
-    coords = list(map(lambda c: Node(*map(float, c.split(','))), args.coord))
+    coords = [args.coord[i:i + 2] for i in range(0, len(args.coord), 2)]
+    coords = list(map(lambda c: Node(*c), coords))
     bbox = Bbox.from_nodes(coords[0], coords[1])
     if args.tags:
-        tags = map(lambda k, v: Tag(key=k, value=v), map(lambda kv: kv.split('=', 1), args.tags.split(',')))
-        #walker = OsmObjectWalker(Tile(image_api=WmsApi(), bbox=bbox), Tag(key='public_transport', value='platform'), square_image_length=100)
+        tags = map(lambda kv: Tag(key=kv[0], value=kv[1]), map(lambda kv: kv.split('=', 1), args.tags.split(',')))
         walker = OsmObjectWalker(Tile(image_api=WmsApi(), bbox=bbox), tags, square_image_length=100)
     else:
         walker = CoordWalker(Tile(image_api=WmsApi(), bbox=bbox), coords, square_image_length=100)
@@ -39,14 +39,15 @@ if __name__ == "__main__":
 
     parser.add_argument(
         'coord',
-        type=str,
+        type=float,
         action='store',
         nargs='+',
-        help='lon,lat coord in WGS84, if --tags bbox left,bottom right,top, else list of coords to fetch.')
+        help='List of lon lat coord in WGS84, if --tags bbox left,bottom right,top, else list of coords to fetch.')
 
     args = parser.parse_args()
     main(args)
 
 # mapproxy-util serve-develop mapproxy.yml
+# python fetch.py --tags public_transport=platform 2.3681867122650146 48.87587197694874 2.3733580112457275 48.8794564363519
 # montage *.png -geometry 100x100+1+1 out.png
 # python retrain.py --image_dir retrain-data --print_misclassified_test_images
